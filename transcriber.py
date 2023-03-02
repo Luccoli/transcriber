7bc2288732e8e0965ad9c22da070a9759531ed8c
Luca Piccoli <luca.piccoli@zextras.com>
2023-03-02 16:24:11 +0100

    Added single function to validate files, single function to create folders. Fixed a regex

--- transcriber.py
+++ transcriber.py
@@ -17,16 +17,15 @@ mimetypes.init()
 
 # FUTURE IDEAS
 # add dry-run option
-# create single validation function accessed by the valid_a and valid_v functions
 # add argument for model option for whisper
 # remove all strings from this file and put them in a separated file
 # remove all global variables from this file and put them in a separated file
 # add possibility to choose the placeholders in output
 # add the possibility to output as plain text
-# checck line 207
+# checck line 181
 
 # strings given in output
-audio_kept_1 = 'you can find the audio file in the folder: '  # NEEDS TO BE REFACTORED AS A BETTER OUTPUT CAN BE CREATED
+audio_kept_1 = 'you can find the audio file in the folder: '
 audio_kept_2 = ' with the name: '
 file_correct = 'the file provided is correct'
 video_incorrect = 'the file provided is not a video file'
@@ -55,8 +54,8 @@ temp_folder = ''
 dest_folder = ''
 
 # files
-file_name = ''  # NO FORMAT
-path_to_file = ''  # ABSOLUTE PATH
+file_name = ''
+path_to_file = ''
 audio_format = '.mp3'
 temporary_audio = ''
 output_prefix = 'transcription_'
@@ -64,6 +63,78 @@ output_name = ''
 
 # whisper model used
 mod = 'large'
+
+def config(test):
+    print('TEST')
+    # folders creation START
+    global temp_folder, dest_folder, main_folder
+
+    main_folder = os.path.join(home_folder, main_folder_name)
+    if not os.path.exists(main_folder):
+        os.mkdir(main_folder)
+        # print('success', main_folder)
+
+    temp_folder = os.path.join(main_folder, tfolder_name)
+    if not os.path.exists(temp_folder):
+        os.mkdir(temp_folder)
+        # print('success', temp_folder)
+
+    dest_folder = os.path.join(main_folder, dfolder_name)
+    if not os.path.exists(dest_folder):
+        os.mkdir(dest_folder)
+        # print('success', dest_folder)
+    # folders creation END
+
+    # path extraction START
+    try_grep = re.search('(?<=name=\').+?(?=\')', str(test.input[0]))
+    vid = pathlib.Path(__file__).parent / str(try_grep.group(0))
+    global path_to_file
+    path_to_file = str(vid.resolve())
+    # path extraction END
+
+    # file_name extraction START
+    g = re.search('[ \+0-9a-zA-Z.-]+(?=\\.)', path_to_file)
+    global file_name
+    file_name = str(g.group(0))
+    global temporary_audio
+    temporary_audio = file_name + audio_format
+    # file_name extraction END
+
+    # output name construction START
+    global output_name
+    output_name = output_prefix + file_name + '.json'
+    # output name construction END
+
+
+def valid(test,origin):
+    print('test')
+    config(test)
+
+    mimestart = mimetypes.guess_type(path_to_file)[0]
+    if origin == 'v':
+        # validation START
+
+        if mimestart is not None:
+            mimestart = mimestart.split('/')[0]
+
+            if mimestart in ['video']:
+                print(file_correct)
+                chang(path_to_file)
+                keep(test)
+            else:
+                parser.error(video_incorrect)
+        # validation END
+    elif origin == 'a':
+        # validation START
+
+        if mimestart is not None:
+            mimestart = mimestart.split('/')[0]
+            if mimestart in ['audio']:
+                print(file_correct)
+                trns(path_to_file)
+            else:
+                parser.error(audio_incorrect)
+        # validation END
 
 
 def keep(test_k):
@@ -79,111 +150,14 @@ def keep(test_k):
 
 
 def valid_v(test_v):
-    # folders creation START
-    global temp_folder, dest_folder, main_folder
-
-    main_folder = os.path.join(home_folder, main_folder_name)
-    if not os.path.exists(main_folder):
-        os.mkdir(main_folder)
-        # print('success', main_folder)
-
-    temp_folder = os.path.join(main_folder, tfolder_name)  # creation here is not possible
-    if not os.path.exists(temp_folder):
-        os.mkdir(temp_folder)
-        # print('success', temp_folder)
-
-    dest_folder = os.path.join(main_folder, dfolder_name)  # most likely same problem here
-    if not os.path.exists(dest_folder):
-        os.mkdir(dest_folder)
-        # print('success', dest_folder)
-    # folders creation END
-
-    # path extraction START
-    try_grep = re.search('(?<=name=\').+?(?=\')', str(test_v.input[0]))
-    vid = pathlib.Path(__file__).parent / str(try_grep.group(0))
-    global path_to_file
-    path_to_file = str(vid.resolve())
-    # path extraction END
-
-    # file_name extraction START
-    g = re.search('[ 0-9a-zA-Z.-+]+(?=\\.)', path_to_file)
-    global file_name
-    file_name = str(g.group(0))
-    global temporary_audio
-    temporary_audio = file_name + audio_format
-    # file_name extraction END
-
-    # output name construction START
-    global output_name
-    output_name = output_prefix + file_name + '.json'
-    # output name construction END
-
-    # validation START
-    mimestart = mimetypes.guess_type(path_to_file)[0]
-
-    if mimestart is not None:
-        mimestart = mimestart.split('/')[0]
-
-        if mimestart in ['video']:
-            print(file_correct)
-            chang(path_to_file)
-            keep(test_v)
-        else:
-            parser.error(video_incorrect)
-    # validation END
+    origin = 'v'
+    print(origin)
+    valid(test_v,origin)
 
 
 def valid_a(test_a):
-    # folders creation START
-    global temp_folder, dest_folder, main_folder
-
-    main_folder = os.path.join(home_folder, main_folder_name)
-    if not os.path.exists(main_folder):
-        os.mkdir(main_folder)
-        # print('success', main_folder)
-
-    temp_folder = os.path.join(main_folder, tfolder_name)  # creation here is not possible
-    if not os.path.exists(temp_folder):
-        os.mkdir(temp_folder)
-        # print('success', temp_folder)
-
-    dest_folder = os.path.join(main_folder, dfolder_name)  # most likely same problem here
-    if not os.path.exists(dest_folder):
-        os.mkdir(dest_folder)
-        # print('success', dest_folder)
-    # folders creation END
-
-    # path extraction START
-    try_grep = re.search('(?<=name=\').+?(?=\')', str(test_a.input[0]))
-    aud = pathlib.Path(__file__).parent / str(try_grep.group(0))
-    global path_to_file
-    path_to_file = str(aud.resolve())
-    # path extraction END
-
-    # file_name extraction START
-    g = re.search('[ 0-9a-zA-Z.-]+(?=\\.)', path_to_file)
-    global file_name
-    file_name = str(g.group(0))
-    global temporary_audio
-    temporary_audio = file_name + audio_format
-    # file_name extraction END
-
-    # output name construction START
-    global output_name
-    output_name = output_prefix + file_name + '.json'
-    # output name construction END
-
-    # validation START
-    mimestart = mimetypes.guess_type(path_to_file)[0]
-
-    if mimestart is not None:
-        mimestart = mimestart.split('/')[0]
-        if mimestart in ['audio']:
-            print(file_correct)
-            trns(path_to_file)
-        else:
-            parser.error(audio_incorrect)
-    # validation END
+    origin = 'a'
+    valid(test_a,origin)
 
 
 def trns(audio_f):
