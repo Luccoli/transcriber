15ae7b8d6bae23f2439e0fea17a7454317830451
Luca Piccoli <luca.piccoli@zextras.com>
2023-03-22 12:45:04 +0100

    feat: added dedicated file for all the strings given in output

---
+++ transcriber/strings_en.py
@@ -0,0 +1,18 @@
+# strings given in output
+audio_kept_1 = 'you can find the audio file in the folder: '
+audio_kept_2 = ' with the name: '
+file_correct = 'the file provided is correct'
+video_incorrect = 'the file provided is not a video file'
+audio_incorrect = 'the file provided is not an audio file'
+proc_start = 'processing initiated:'
+proc_end = 'processing completed:'
+output_loc_1 = 'you can find the transcription in the folder: '
+output_loc_2 = ' with the name: '
+
+# strings in the usage/help
+desc_gen = 'transcribe a media file'
+help_audio_parser = 'command to transcribe an audio file'
+help_audio_input = 'use this flag to provide an audio file'
+help_video_parser = 'command to transcribe a video file'
+help_video_input = 'use this flag to provide a video file'
+help_video_keep = 'use this flag if you want to keep the temporary audio file created - False by default'
